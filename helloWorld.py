ae62614782d0d2295afac212e8eca93c5512f894
Kate Gordon <kategordon.code@gmail.com>
2019-08-30 15:46:11 -0400

    Added first Hello World
    git status

---
+++ helloWorld.py
@@ -0,0 +1,11 @@
+name = input('What is your name? ')
+
+print ("Hello" + ", " + name + "!")
+
+print ("HELLO" + ", " + name.upper() + "!")
+
+print("YOUR NAME HAS " + str(len(name)) + " LETTERS IN IT! AWESOME!")
+
+aNum = input("Enter number: ")
+
+print(int(aNum) * int(aNum))
